36c7519e43690c13008ac0905b45182785c4fce9
ThalesBonel <thalesbbonel@gmail.com>
2025-05-08 12:35:55 -0300

    more scenarios and refactoring old scenario

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -23,73 +23,154 @@ class RegisterForm {
     this.elements.submitBtn().click()
   }
 
-}
-
-
+} 
 
 const registerForm = new RegisterForm()
 const colors = {
-  erros: 'rgb(220, 53, 69)',
-  success: 'rgb(222, 226, 230)'
+  error: 'rgb(220, 53, 69)',
+  success: 'rgb(220, 53, 69)'
 }
 
 describe('Image Registration', () => {
-  describe('Submitting an image with invalid inputs', () => {
+  // describe.skip('Submitting an image with invalid inputs', () => {
+  //   after(() => {
+  //     cy.clearAllLocalStorage()
+  //   })
+
+  //   const input = {
+  //     title: '',
+  //     url:''
+  //   }
+    
+  //   it('Given I am on the image registration page', () => {
+  //     cy.visit('/')
+  //   }) 
+  //   it (`When I enter "${input.title}" in the title field`, () => {
+  //     registerForm.typeTitle(input.title)
+  //   })
+  //   it (`When I enter "${input.url}" in the URL field`, () => {
+  //     registerForm.typeUrl(input.url)
+  //   })
+  //   it (`Then I click the submit button`, () => {
+  //     registerForm.clickSubmit()
+  //   })
+  //   it (`Then I should see "Please type a title for the image" message above the title field`, () => {
+  //     registerForm.elements.titleFeedback().should('contains.text', 'Please type a title for the image.')
+  //     // registerForm.elements.titleFeedback().should(element => {
+  //     //   debugger
+  //     // })
+  //   }) 
+  //   it (`And I should see "Please type a valid URL" message above the imageUrl field`, () => {
+  //     registerForm.elements.imageURLFeedback().should('contains.text', 'Please type a valid URL')
+  //   })
+  //   it (`And I should see an exclamation icon in the title and URL fields`, () => {
+  //     registerForm.elements.titleFeedback().should(([element]) => {
+  //       const styles = window.getComputedStyle(element)
+  //       const border = styles.getPropertyValue('border-right-color')
+  //       //debugger
+  //       assert.strictEqual(border, colors.error)
+  //     })
+  //   })
+  // })
+
+  // // >>> REFATORANDO TESTE ACIMA
+  describe('Given i am on the image registration page', () => {
+    beforeEach(() => {
+      cy.visit('/')
+    })
+
+    after(() => {
+      cy.clearAllLocalStorage()
+    })
+
+    context('When I submit with invalid inputs', () => {
+      beforeEach(() => {
+        registerForm.typeTitle('')
+        registerForm.typeUrl('')
+        registerForm.clickSubmit()
+      });
+    });
+
+    it('Then I should see validation messages', () => {
+      registerForm.elements.titleFeedback().should('contain.text', 'Please type a title for the image.')
+      registerForm.elements.imageURLFeedback().should('contain.text', 'Please type a valid URL')
+    })
+
+    it('And I should see error icons (red border)', () => {
+      registerForm.elements.titleFeedback().should(([el]) => {
+        const styles = window.getComputedStyle(el)
+        const border = styles.getPropertyValue('border-right-color')
+        assert.strictEqual(border, colors.error)
+      })
+    })
+  });
+
+  describe('Submitting an image with valid inputs using enter key', () => {
     after(() => {
       cy.clearAllLocalStorage()
     })
 
     const input = {
-      title: '',
-      url:''
+      title: 'Alien BR',
+      url: 'https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg'
     }
 
     it('Given I am on the image registration page', () => {
-     cy.visit('/')
-    }) 
-
-    it (`When I enter "${input.title}" in the title field`, () => {
-      registerForm.typeTitle(input.title)
-    })
-    it (`When I enter "${input.url}" in the URL field`, () => {
-      registerForm.typeUrl(input.url)
-    })
-    it (`Then I click the submit button`, () => {
-      registerForm.clickSubmit()
-    })
-
-    it (`Then I should see "Please type a title for the image" message above the title field`, () => {
-      registerForm.elements.titleFeedback().should('contains.text', 'Please type a title for the image.')
-      // registerForm.elements.titleFeedback().should(element => {
-      //   debugger
-      // })
-    }) 
-    it (`And I should see "Please type a valid URL" message above the imageUrl field`, () => {
-      registerForm.elements.imageURLFeedback().should('contains.text', 'Please type a valid URL')
-    })
-    it (`And I should see an exclamation icon in the title and URL fields`, () => {
-      registerForm.elements.titleFeedback().should(([element]) => {
-        const styles = window.getComputedStyle(element)
-        const border = styles.getPropertyValue('border-right-color')
-        assert.strictEqual(border, colors.erros)
-      })
-    })
-      
-
-  })
-
-
-  describe.skip('Submitting an image with valid inputs using enter key', () => {
-    it('Given I am on the image registration page', () => {
-      
+      cy.visit('/')
     });
 
-    it(' When I enter "Alien BR" in the title field',)
-    it('',)
-    it('',)
-    it('',)
-    it('',)
-    it('',)
+    it(`When I enter "${input.title}" in the title field`, () => {
+      registerForm.clickSubmit()
+      registerForm.typeTitle(input.title)
+    })
+     
+    it(`When I enter "${input.url}" in the URL field`, () => {
+      registerForm.typeUrl(input.url)
+    })
+    it('Then I should see a check icon in the title field', () => {
+      registerForm.elements.titleFeedback().should(([element]) => {
+        const styles = window.getComputedStyle(element)
+        const border = styles.getPropertyValue('color')
+        //debugger
+        assert.strictEqual(border, colors.success)
+      })
+    })
+    it('Then I should see a check icon in the imageUrl field', () => {
+      registerForm.elements.imageURLFeedback().should(([element]) => {
+        const styles = window.getComputedStyle(element)
+        const border = styles.getPropertyValue('color')
+        //debugger
+        assert.strictEqual(border, colors.success)
+      })
+    })
+    it('Then I can hit enter to submit the form', () => {
+      registerForm.clickSubmit()
+    })
+    it('And the list of registered images should be updated with the new item', () => {
+      cy.get('#card-list .card-img').should((elements) => {
+        const lastElement = elements[elements.length - 1]
+        const src = lastElement.getAttribute('src')
+        assert.strictEqual(src, input.url)
+      })
+
+    })
+    it('And the new item should be stored in the localStorage', () => {
+      cy.getAllLocalStorage().should((ls) => {
+        const currentLs = ls[window.location.origin]
+        const elements = JSON.parse(Object.values(currentLs))
+        const lastElement = elements[elements.length - 1]
+
+        assert.deepStrictEqual(lastElement, {
+          title: input.title,
+          imageUrl: input.url,
+        })
+      })
+    })
+
+    it('Then The inputs should be cleared', () => {
+      registerForm.elements.titleInput().should('have.value', '')
+      registerForm.elements.imageURLInput().should('have.value', '')
+    })
 
   });
 })
